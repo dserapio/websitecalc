4effeadcf488bc05fcdc79aa40f8933f787d6f32
sechevarria98 <sechevarria2@gmail.com>
2020-04-25 23:22:25 -0500

    Adding map integration

--- website/src/components/FindRecycler.js
+++ website/src/components/FindRecycler.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import { GoogleMap, withScriptjs, withGoogleMap } from "react-google-maps"
 import '../App.css';
- 
+
+function Map() {
+  return (
+    <GoogleMap 
+      defaultZoom={10}
+      defaultCenter={{ lat: 41.878113, lng: -87.629799}}
+    />
+  );
+}
+
+const WrappedMap = withScriptjs(withGoogleMap(Map))
+
 export default function FindRecycler() {
-    return (
-       <div>
-          <h1>Contact US</h1>
-          <p>Contact US page body content</p>
-       </div>
-    );
+  return (
+    <div style={{ width: "100vw", height: "100vh"}}>
+      <WrappedMap
+        googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&
+        libraries=geometry,drawing,places&key=${process.env.REACT_APP_GOOGLE_KEY}`} 
+        loadingElement={<div style={{ height: "100%"}} />}
+        containerElement={<div style={{ height: "100%"}} />}
+        mapElement={<div style={{ height: "100%"}} />}
+      />
+    </div>
+  )
 }
