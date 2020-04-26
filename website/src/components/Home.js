c287fcbf3febe3a45e801a16b98541ec1a1feb5c
sechevarria98 <sechevarria2@gmail.com>
2020-04-26 01:44:51 -0500

    Homepage

--- website/src/components/Home.js
+++ website/src/components/Home.js
@@ -6,13 +6,41 @@ export default function Home () {
    const images = [
       'https://s3files.core77.com/blog/images/lead_n_spotlight/477276_title__52109_cnNbfrAUP.jpg',
       'https://icdn2.digitaltrends.com/image/digitaltrends/technological-waste.jpg',
-      'https://media.npr.org/assets/artslife/arts/2010/12/electronic-computer-waste/jim-puckett-096290b7435a25075e8cfdf71ff79eb5240e1f01-s800-c85.jpg',
-      'https://images.unsplash.com/photo-1534161308652-fdfcf10f62c4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2174&q=80'
-    ]
+      'https://media.npr.org/assets/artslife/arts/2010/12/electronic-computer-waste/jim-puckett-096290b7435a25075e8cfdf71ff79eb5240e1f01-s800-c85.jpg'    ]
 
     return (
        <div>
+          <p className="statement">
+          e-Stewards is the 
+          globally responsible way to 
+          recycle your electronics.
+          </p>
+
           <Slider slides={images}/>
+
+         <div className="biggerbox">
+            <div className="bigbox">
+               <p className="littlebox">
+               FOR ENTERPRISES
+               Use the highest industry 
+               standard for your asset 
+               disposition
+               </p>
+
+               <p className="littlebox">
+               FOR RECYCLERS
+               Differentiate your business 
+               with the e-Stewards Standard.
+               </p>
+
+               <p className="littlebox">
+               FOR CONSUMERS
+               Join us today in creating a 
+               cleaner, more just world.
+               </p>
+            </div>
+         </div>
+          
        </div>
     );
 }
