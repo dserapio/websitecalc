5729ae3c45aff5d50f0a8fadc2522e6d85304d78
sechevarria98 <samechevarria2@gmail.com>
2020-05-20 17:17:12 -0500

    Updated Styling

--- website/src/components/Calculator.js
+++ website/src/components/Calculator.js
@@ -5,12 +5,36 @@ export default function Calculator() {
     return (
        <div className="content">
          <form>
-            <input className="TextField" defaultValue="Total Laptops" />
-            <input className="TextField" defaultValue="Total Desktop and Servers" />
-            <input className="TextField" defaultValue="Total Flat Panel Displats (LCDs)" />
-            <input className="TextField" defaultValue="Total Mobile Phones" />
-            <input className="TextField" defaultValue="Total Imaging Devices" />
-            <input className="TextField" defaultValue="Total Others(Mice, Keyboards, etc.)" />
+            <label for="laptops"> 
+               Total Laptops 
+               <input className="TextField" type="text" name="Laptop"/>
+            </label>
+
+            <label for="desktops"> 
+               Total Desktops and Servers 
+               <input className="TextField" type="text" name="Desktops" />   
+            </label>
+
+            <label for="lcd"> 
+               Total Flat Panel Displays (LCDs) 
+               <input className="TextField" type="text" name="LCD"/>
+            </label>
+
+            <label for="phones"> 
+               Total Mobile Phones 
+               <input className="TextField" type="text" name="Phones" />
+            </label>
+
+            <label for="image"> 
+               Total Imaging Devices 
+               <input className="TextField" type="text" name="Image"/>
+            </label>
+
+            <label for="others"> 
+               Total Others(Mice, Keyboards, etc.) 
+               <input className="TextField" type="text" name="Others"/>
+            </label>
+
          </form>
 
          <button>Calculate</button>
