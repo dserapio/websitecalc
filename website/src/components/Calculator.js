5105e3b79b77a06fcc3f19d39e153eb827de543f
Your Name <noahluvsmilkduds@gmail.com>
2020-05-20 17:45:34 -0500

    styling changes

--- website/src/components/Calculator.js
+++ website/src/components/Calculator.js
@@ -10,7 +10,7 @@ export default function Calculator() {
       return setEnter(enter => !enter);
    };
    const onAbout = () => setAbout(about => !about);
-   const aboutButt = <button type="button" onClick={onAbout}>About</button>;
+   const aboutButt = <button className="page-link" type="button" onClick={onAbout}>About</button>;
 
    return (
       <div className="content">
@@ -30,59 +30,61 @@ export default function Calculator() {
 }
 
 const Input = (props) => (
-   <div>
+   <section>
       <h1>Find Out Material Yields</h1>
       <p>Enter in any electronic, and we'll breakdown what it's made of</p>
       <form>
          <label for="laptops"> 
             Total Laptops 
-            <input className="TextField" type="text" name="Laptop"/>
+            <input className="textfield" type="text" name="Laptop"/>
          </label>
 
          <label for="desktops"> 
             Total Desktops and Servers 
-            <input className="TextField" type="text" name="Desktops" />   
+            <input className="textfield" type="text" name="Desktops" />   
          </label>
 
          <label for="lcd"> 
             Total Flat Panel Displays (LCDs) 
-            <input className="TextField" type="text" name="LCD"/>
+            <input className="textfield" type="text" name="LCD"/>
          </label>
 
          <label for="phones"> 
             Total Mobile Phones 
-            <input className="TextField" type="text" name="Phones" />
+            <input className="textfield" type="text" name="Phones" />
          </label>
 
          <label for="image"> 
             Total Imaging Devices 
-            <input className="TextField" type="text" name="Image"/>
+            <input className="textfield" type="text" name="Image"/>
          </label>
 
          <label for="others"> 
             Total Others(Mice, Keyboards, etc.) 
-            <input className="TextField" type="text" name="Others"/>
+            <input className="textfield" type="text" name="Others"/>
          </label>
       </form>
 
       <button type="button" onClick={props.funct}>Calculate</button>
-   </div>
+   </section>
 )
 
 const About = (props) => (
    <div>
-      <button onClick={props.funct}>Calculator</button>
-      <h1>About the Calculator</h1>
-      <p>
-         Welcome to the e-Stewards Global Impact Calculator. This tool is designed to estimate
-         the environmental benefits of recycling your electronic waste with e-Stewards Certified Recyclers.
-      </p>
-      <h2>The environmental benefits are measured in three critical areas:</h2>
-      <ol>
-         <li>Green house gas avoidance</li>
-         <li>Toxic metals diverted from export, dumping or disposal</li>
-         <li>Valuable metals diverted from landfill or disposal</li>
-      </ol>
-      <p>These figures can be used to demonstrate and measure individual or corporate responsibility</p>
+      <button className="page-link" onClick={props.funct}>Calculator</button>
+      <section>
+         <h1>About the Calculator</h1>
+         <p>
+            Welcome to the e-Stewards Global Impact Calculator. This tool is designed to estimate
+            the environmental benefits of recycling your electronic waste with e-Stewards Certified Recyclers.
+         </p>
+         <h2>The environmental benefits are measured in three critical areas:</h2>
+         <ol>
+            <li>Green house gas avoidance</li>
+            <li>Toxic metals diverted from export, dumping or disposal</li>
+            <li>Valuable metals diverted from landfill or disposal</li>
+         </ol>
+         <p>These figures can be used to demonstrate and measure individual or corporate responsibility</p>
+      </section>
    </div>
 );
