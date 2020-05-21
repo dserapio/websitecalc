b2588527fb0637c0a3378eb1954439886e7d21ba
sechevarria98 <samechevarria2@gmail.com>
2020-05-21 18:42:51 -0500

    Updated Map

--- website/src/components/FindRecycler.js
+++ website/src/components/FindRecycler.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import GoogleMapReact from "google-map-react"
 import '../App.css';
 
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
 class FindRecycler extends Component {
   static defaultProps = {
     center: {
