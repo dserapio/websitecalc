fb579168c3ebc8650a87953158c7a62aaf0cca7a
sechevarria98 <samechevarria2@gmail.com>
2020-05-24 22:28:00 -0500

    Location data along with map markers

--- website/src/components/FindRecycler.js
+++ website/src/components/FindRecycler.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react';
 import GoogleMapReact from "google-map-react"
+import locationData from '../data/location-info.json'
 import '../App.css';
+
+const Marker = (props) => {
+  const { locationName } = props;
+  return (
+    <div 
+      className="marker" 
+      title = { locationName }
+    />
+  );
+};
 
 class FindRecycler extends Component {
   static defaultProps = {
@@ -8,18 +19,28 @@ class FindRecycler extends Component {
       lat: 41.878113,
       lng: -87.629799
     },
-    zoom: 11
+    zoom: 10
   };
 
-  render() {
+  render () {
     return (
       <div className="map">
         <GoogleMapReact
           bootstrapURLKeys={{ key: "AIzaSyA-U9tVnswDVHRfHH3DLhc-Y8HWFksQyNQ" }}
-          defaultCenter={this.props.center}
-          defaultZoom={this.props.zoom}
+          defaultCenter={ this.props.center }
+          defaultZoom={ this.props.zoom }
         >
-          
+            {locationData.map((loc, i) => {
+              return (
+                <Marker
+                  key = {i}
+                  lat = { loc.location[0] }
+                  lng = { loc.location[1] }
+                  locationName = { loc.name }
+                  locationPhone = {loc.phone}
+                />
+              );
+            })} 
         </GoogleMapReact>
       </div>
     );
