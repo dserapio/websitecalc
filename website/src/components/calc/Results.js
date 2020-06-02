8415f84f4f0de51e2f069f45042cca519a4bd1d0
sechevarria98 <sechevarria2@gmail.com>
2020-06-02 18:34:34 -0500

    output styling change

--- website/src/components/calc/Results.js
+++ website/src/components/calc/Results.js
@@ -44,57 +44,58 @@ const Results = ({toAbout, toBack, values}) => {
 
       <section className="main">
          <h1>Total Material Yields</h1>
-         <table>
-            <thead>
-               <tr>
-                  <th className="output">Material</th>
-                  <th className="output">Amount Yield ({unit.name})</th>
-               </tr>
-            </thead>
-
-            <tbody>
-               {Object.entries(values).map(([name, value], i) => (
-                  <tr key={name+i}>
-                     <td className="output">{name} <div className="color-box" style={{ backgroundColor: getColor(i) }}></div></td>
-                     <td className="output-value">{(value * unit.convert).toFixed(4)} {unit.name} </td>
+         <div className="outputs"> 
+            <table>
+               <thead>
+                  <tr>
+                     <th className="output">Material</th>
+                     <th className="output">Amount Yield ({unit.name})</th>
                   </tr>
-               ))}
-            </tbody>
-         </table>
+               </thead>
 
-            <div className="buttons">
-               <button type="button" onClick={toBack}>Back</button>
-               <button
-                  className={activeAttr('kg')}
-                  type="button" 
-                  onClick={changeTokg}>kg</button>
-               <button
-                  className={activeAttr('lbs.')}
-                  type="button"
-                  onClick={changeTolbs}>lbs</button>
-            </div>
+               <tbody>
+                  {Object.entries(values).map(([name, value], i) => (
+                     <tr key={name+i}>
+                        <td className="output">{name} <div className="color-box" style={{ backgroundColor: getColor(i) }}></div></td>
+                        <td className="output-value">{(value * unit.convert).toFixed(4)} {unit.name} </td>
+                     </tr>
+                  ))}
+               </tbody>
+            </table>
 
-            <div className="piechartbox">
-               <PieChart 
-                  data={pieData}
-                  className="piechart"
-                  radius={PieChart.defaultProps.radius - 6}
-                  lineWidth={60}
-                  segmentsStyle={{ transition: 'stroke .10s', cursor: 'pointer' }}
-                  segmentsShift={ (index) => (index === selected ? 6 : 1) }
-                  animate
-                  label={({ dataEntry }) => Math.round(dataEntry.percentage) + '%'}
-                  labelStyle={{
-                     fill: '#fff',
-                     opacity: 0.75,
-                     pointerEvents: 'none',
-                  }}
-                  labelPosition = {110 - lineWidth / 2}
-                  onClick={(_, index) => {
-                     setSelected(index === selected ? undefined : index);
-                  }}
-               />
-            </div>
+            <PieChart 
+               data={pieData}
+               className="piechart"
+               radius={PieChart.defaultProps.radius - 6}
+               lineWidth={60}
+               segmentsStyle={{ transition: 'stroke .10s', cursor: 'pointer' }}
+               segmentsShift={ (index) => (index === selected ? 6 : 1) }
+               animate
+               label={({ dataEntry }) => Math.round(dataEntry.percentage) + '%'}
+               labelStyle={{
+                  fill: '#fff',
+                  opacity: 0.75,
+                  pointerEvents: 'none',
+               }}
+               labelPosition = {110 - lineWidth / 2}
+               onClick={(_, index) => {
+                  setSelected(index === selected ? undefined : index);
+               }}
+            />
+
+         </div>
+
+         <div className="buttons">
+            <button type="button" onClick={toBack}>Back</button>
+            <button
+               className={activeAttr('kg')}
+               type="button" 
+               onClick={changeTokg}>kg</button>
+            <button
+               className={activeAttr('lbs.')}
+               type="button"
+               onClick={changeTolbs}>lbs</button>
+         </div>
       </section>
    </>;
  };
